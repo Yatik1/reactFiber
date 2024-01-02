9ff3548fe59a869eb6d0b730b11d772cf0653920
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-01-02 19:07:27 +0530

    ::component

--- web3fiber/src/components/Experience.jsx
+++ web3fiber/src/components/Experience.jsx
@@ -1,4 +1,6 @@
-import { OrbitControls, Text } from "@react-three/drei";
+import { Environment, OrbitControls, Text } from "@react-three/drei";
+import { Camping } from "./Camping";
+import { degToRad } from "three/src/math/MathUtils";
 
 export const Experience = () => {
   return (
@@ -10,10 +12,18 @@ export const Experience = () => {
          position-y={-0.5}
          position-z={1}
          lineHeight={0.8}
-         textAlign="center">
+         textAlign="center"
+         rotation-y={degToRad(30)}
+         anchorY={"bottom"}
+        >
+
          MY LITTLE{"\n"}CAMPING
          <meshBasicMaterial color='white' />
       </Text>
+      <group rotation-y={degToRad(-25)} position-x={3}>
+        <Camping scale={0.6} />
+      </group>
+      <Environment preset="sunset" />
     </>
   );
 };
