397f245b6324c18a0823c506975bf61c5a40421a
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-01-02 19:07:17 +0530

    :: camera position

--- web3fiber/src/App.jsx
+++ web3fiber/src/App.jsx
@@ -3,8 +3,8 @@ import { Experience } from "./components/Experience";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
+    <Canvas shadows camera={{ position: [0,0,8], fov: 42 }}>
+      <color attach="background" args={["#171720"]} />
       <Experience />
     </Canvas>
   );
